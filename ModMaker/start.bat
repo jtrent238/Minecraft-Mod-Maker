2753ecfd40cda1db301c459189ffb7340b197092
Trent Patterson <jtrent238@outlook.com>
2018-11-19 16:38:05 -0500

    11192018438

--- ModMaker/start.bat
+++ ModMaker/start.bat
@@ -1,4 +1,15 @@
 @echo off
 cls
+set /p pip_updated=<pip_updated.txt
+
+if %pip_updated% == true (
+	echo PIP seems to already be updated!
+)
+else %pip_updated% == false (
+	echo updating PIP...
+	python -m pip install --upgrade pip
+	pip updated!
+	echo true > pip_updated.txt
+)
 py Main.py
 pause
